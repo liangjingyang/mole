e27189004d35d320f14e6bf17e11ed046a766f2a
liangjingyang <ljy0922@gmail.com>
2014-01-09 16:54:57 +0800

    fix bug

--- include/mole.hrl
+++ include/mole.hrl
@@ -1,12 +1,10 @@
 
 -define(APP, mole).
 
--define(SERVER_REQ, 1).
--define(SERVER_RES, 2).
--define(WAN_CONN, 3).
--define(LAN_CONN, 4).
--define(BCAST_CONN, 5).
--define(P2P_HEARTBEAT, 6).
+-define(SERVER_REQ, "1").
+-define(SERVER_RES, "2").
+-define(WAN_CONN, "3").
+-define(LAN_CONN, "4").
 
 -define(ETS_MOLE, ets_mole).
 
